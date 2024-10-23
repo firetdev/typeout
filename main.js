4a4949e5d3df25b0d5b716e3b355fc2778ad1482
flesheatindragon <futuregamesstore@gmail.com>
2024-10-23 17:31:42 -0500

    changes

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-/*
+/**
 Message: the text you want typed out
 lps: the number of letters to type per second
 id: id of the HTML tag you are typing it into
 */
+
 var inTag = false;
+
 function typeText(message, lps, id) {
     if (inTag) {
-        document.getElementById(id).innerHTML = document.getElementById(id).innerHTML.replace(new RegExp("<\/[a-zA-Z][^>]*>", "g"), "");
+        document.getElementById(id).innerHTML = document.getElementById(id).innerHTML.replace(new RegExp('<\/[a-zA-Z][^>]*>', 'g'), '');
     }
-    if (message == "")
+    if (message == '')
         return;
-    if (message[0] == "<") {
-        if (message[1] != "b")
+    if (message[0] == '<') {
+        if (message[1] != 'b')
             inTag = !inTag;
         var tagRegex = /<\/?[a-zA-Z][^>]*>/;
         var tag = message.match(tagRegex)[0];
         document.getElementById(id).innerHTML += tag;
-        message = message.replace(tagRegex, "");
+        message = message.replace(tagRegex, '');
     }
-    if (message[0] == " ") {
-        document.getElementById(id).innerHTML += "&nbsp;";
+    if (message[0] == ' ') {
+        document.getElementById(id).innerHTML += '&nbsp;';
     } else {
         document.getElementById(id).innerHTML += message[0];
     }
     message = message.slice(1);
-    window.setTimeout("typeText('" + message + "', " + lps + ", '" + id +"')", 1000/lps);
+    setTimeout("typeText('" + message + "', " + lps + ", '" + id +"')", 1000/lps);
 }
